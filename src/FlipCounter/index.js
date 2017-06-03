ffa781d709f10952c6f8234efc55cb259358363a
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-06-03 20:36:34 +0200

    Fill counter with leading zeros.

--- src/FlipCounter/index.js
+++ src/FlipCounter/index.js
@@ -1,99 +1,115 @@
 import $ from 'jquery';
 
 export default class FlipCounter {
-  constructor(initValue) {
-    this.initialValue = initValue;
-    this.init(this.initialValue)
+  constructor(minValue, maxValue) {
+    this._currentValue = 0;
+    this.MIN_VALUE = minValue;
+    this.MAX_VALUE = maxValue;
   }
   
-  init(value) {
-    if(!value) {
-      throw new Error("FlipCounter needs to be initialized with a number.")
+  set currentValue(val) {
+    this._currentValue = val;
+  }
+
+  get currentValue() {
+    return this._currentValue;
+  }
+  
+  get flipCounterSettings() {
+    return {
+      MIN_VALUE: this.MIN_VALUE,
+      MAX_VALUE: this.MAX_VALUE
     }
-    if(value > this.MAX_DIGIT_NUMBER) {
-      throw new Error("Initialized number cannot be greater than: " + this.MAX_DIGIT_NUMBER)
-    }
-    $("body").removeClass("play"); // if anything is playing
+  }
     
-    var digitsArray = value.toString().split("").map((digitString) => {
+  init() {
+    $("body").removeClass("play"); // if anything is playing
+    this.currentValue = Math.ceil(Math.random() * this.MAX_VALUE + this.MIN_VALUE)
+    let digitsArray = this.currentValue.toString().split("").map((digitString) => {
       return parseInt(digitString);
     })
+    let currentValueLength = this.currentValue.toString().length
+    let maxValueLength = this.currentValue.toString().length
+    if(currentValueLength < maxValueLength) {
+      // fill the the array with leading zeros if number too small for the counter
+      Array(maxValueLength - currentValueLength).fill(0)
+    }
     this.setFlips(digitsArray);
   }
   
   setFlips(digitsArr) {
-      setUnits();
-      setTens();
-      setHundreds();
-      setThousands();
+    setUnits(digitsArr[0]);
+    setTens(digitsArr[1]);
+    setHundreds(digitsArr[2]);
+    setThousands(digitsArr[3]);
+    
+    function setUnits(val) {
+      var aa = $("ul.units li.active");
+      var nxt;
       
-      function setUnits() {
-          var aa = $("ul.units li.active");
-          var nxt;
-          
-          if (aa.html() == undefined) {
-              aa = $("ul.units li").eq(0);
-          if (digitsArr[3] != 0) {
-              aa.addClass("before")
-                  .removeClass("active");
-          }
-              nxt = $("ul.units li").eq(digitsArr[3]);
-              nxt.addClass("active")
-                  .closest("body")
-                  .addClass("play");
-          }
+      if (aa.html() == undefined) {
+        aa = $("ul.units li").eq(0);
+      if (val != 0) {
+        aa.addClass("before")
+          .removeClass("active");
       }
+        nxt = $("ul.units li").eq(val);
+        nxt.addClass("active")
+          .closest("body")
+          .addClass("play");
+      }
+    }
+    
+    function setTens(val) {    
+      var aa = $("ul.tens li.active");
+      var nxt;
       
-      function setTens() {    
-        var aa = $("ul.tens li.active");
-        var nxt;
-        
-        if (aa.html() == undefined) {
-          aa = $("ul.tens li").eq(0);
-          if (digitsArr[2] != 0) {
-            aa.addClass("before")
-              .removeClass("active");
-          }
-          nxt = $("ul.tens li").eq(digitsArr[2]);
-          nxt.addClass("active")
-            .closest("body")
-            .addClass("play");
+      if (aa.html() == undefined) {
+        aa = $("ul.tens li").eq(0);
+        if (val != 0) {
+          aa.addClass("before")
+            .removeClass("active");
         }
+        nxt = $("ul.tens li").eq(val);
+        nxt.addClass("active")
+          .closest("body")
+          .addClass("play");
       }
-          
-      function setHundreds() {    
-        var aa = $("ul.hundreds li.active");
-        var nxt;
+    }
         
-        if (aa.html() == undefined) {
-          aa = $("ul.hundreds li").eq(0);
-          if (digitsArr[1] != 0) {
-            aa.addClass("before")
-              .removeClass("active");
-          }
-          nxt = $("ul.hundreds li").eq(digitsArr[1]);
-          nxt.addClass("active")
-            .closest("body")
-            .addClass("play");
+    function setHundreds(val) {    
+      var aa = $("ul.hundreds li.active");
+      var nxt;
+      
+      if (aa.html() == undefined) {
+        aa = $("ul.hundreds li").eq(0);
+        if (val != 0) {
+          aa.addClass("before")
+            .removeClass("active");
         }
+        nxt = $("ul.hundreds li").eq(val);
+        nxt.addClass("active")
+          .closest("body")
+          .addClass("play");
       }
-          
-      function setThousands() {    
-        var aa = $("ul.thousands li.active");
-        var nxt;
+    }
         
-        if (aa.html() == undefined) {
-          aa = $("ul.thousands li").eq(0);
-          if (digitsArr[0] != 0) {
-            aa.addClass("before")
-              .removeClass("active");
-          }
-          nxt = $("ul.thousands li").eq(digitsArr[0]);
-          nxt.addClass("active")
-            .closest("body")
-            .addClass("play");
+    function setThousands(val) {    
+      var aa = $("ul.thousands li.active");
+      var nxt;
+      
+      if (aa.html() == undefined) {
+        aa = $("ul.thousands li").eq(0);
+        if (val != 0) {
+          aa.addClass("before")
+            .removeClass("active");
         }
+        nxt = $("ul.thousands li").eq(val);
+        nxt.addClass("active")
+          .closest("body")
+          .addClass("play");
       }
+    }
   }
   
   add(val) {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import $ from 'jquery';
 import FlipCounter from './FlipCounter'
 
-FlipCounter.MAX_DIGIT_NUMBER = 9999;
-FlipCounter.MIN_DIGIT_NUMBER = 1000;
-let initValue = Math.ceil(
-  Math.random() * FlipCounter.MAX_DIGIT_NUMBER + FlipCounter.MIN_DIGIT_NUMBER
-)
-let fc = new FlipCounter(initValue);
+let fc = new FlipCounter(0, 9999);
+fc.init()
 
 // Move to class events
 $(window).click(function() {
-   fc.add(20); 
+  fc.add(20); 
 });
